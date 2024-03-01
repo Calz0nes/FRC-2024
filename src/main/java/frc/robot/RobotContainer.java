7926b5f4565b3ac62b8d52989d82c31b9f94370d
Cadel Conner <cadelconner@gmail.com>
2024-03-01 17:25:30 -0600

    Turtle Mode

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import frc.robot.generated.TunerConstants;
 public class RobotContainer {
   private double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
   private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+  private double TurtleModify = 3;
 
   /* Setting up bindings for necessary control of the swerve drive platform */
   private final CommandXboxController joystick = new CommandXboxController(0); // My joystick
@@ -37,18 +38,25 @@ public class RobotContainer {
   // Assign commands to buttons here.
   private void configureBindings() {
     drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
-        drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with
-                                                                                           // negative Y (forward)
-            .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-            .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+        drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with.
+                                                                                           // negative Y (forward).
+            .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left).
+            .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left).
         ));
 
     joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
     joystick.b().whileTrue(drivetrain
         .applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
 
-    // reset the field-centric heading on left bumper press
+    // reset the field-centric heading on left bumper press.
     joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
+
+    // Turtle Mode (slow).
+    joystick.rightTrigger().whileTrue(drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed/TurtleModify) // Drive forward with.
+                                                                                           // negative Y (forward).
+            .withVelocityY(-joystick.getLeftX() * MaxSpeed/TurtleModify) // Drive left with negative X (left).
+            .withRotationalRate(-joystick.getRightX() * MaxAngularRate/TurtleModify) // Drive counterclockwise with negative X (left).
+    ));
 
     drivetrain.registerTelemetry(logger::telemeterize);
   }
